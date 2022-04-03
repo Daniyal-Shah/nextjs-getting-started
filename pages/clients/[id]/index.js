e154e7b911f78c0b3359e4f100ad52a5a6306f40
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-04-03 23:29:54 +0500

    completed file-based routing module

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useRouter } from "next/router";
 
-function ClientsProjectPage(props) {
+function ClientsProjectPage() {
+  const router = useRouter();
+
+  function buttonHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientProjectId]",
+      query: { id: "dani", clientProjectId: "A" },
+    });
+  }
+
   return (
     <div>
       <h1>Clients Project Page</h1>
+      <button onClick={buttonHandler}>Load Project A</button>
     </div>
   );
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 function ClientsPage(props) {
+  let clients = [
+    { id: "dani", name: "Daniyal" },
+    { id: "bilal", name: "Bilal" },
+  ];
   return (
     <div>
       <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
