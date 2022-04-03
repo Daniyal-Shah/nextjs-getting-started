33a78e2cc3d8ac8b8c3284886978b442bdbcd716
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-04-03 21:42:57 +0500

    Navigation with link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,6 +16,20 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="">Homepage</a>
         </h1>
+        <ul>
+          <li>
+            <Link href="/portfolio">PortFolio</Link>
+          </li>
+          <li>
+            <Link href="/clients">Clients</Link>
+          </li>
+          <li>
+            <Link href="/blogs/a">Blogs</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
       </main>
     </div>
   );
