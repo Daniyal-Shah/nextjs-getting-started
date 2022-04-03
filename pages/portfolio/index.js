7657948e6495583f7591592c1e004a460cce93c5
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-04-03 16:27:47 +0500

    nested route in products

---
+++ pages/portfolio/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PortFolio(props) {
+  return (
+    <div>
+      <h1>
+        <a href="">PortFolio Page</a>
+      </h1>
+    </div>
+  );
+}
+
+export default PortFolio;
---
+++ pages/portfolio/list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function list(props) {
+  return (
+    <div>
+      <h1>Simi, danu</h1>
+    </div>
+  );
+}
+
+export default list;
