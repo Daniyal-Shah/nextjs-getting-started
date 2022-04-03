f4846afdee9d4d12cf0eebeee6adb8fc1af46fd4
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-04-03 19:30:32 +0500

    Nested Dynamic Routing

--- pages/clients/[id]/[clientProjectId].js
+++ pages/clients/[id]/[clientProjectId].js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useRouter } from "next/router";
 
-function PortfolioProjectPage(props) {
+function ClientsSpecificProject(props) {
   const router = useRouter();
 
   console.log(router.query);
-  console.log(router.pathname);
+
   return (
     <div>
-      <h1>PortfolioProjectPage</h1>
+      <h1>Clients Specific Project</h1>
     </div>
   );
 }
 
-export default PortfolioProjectPage;
+export default ClientsSpecificProject;
---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ClientsProjectPage(props) {
+  return (
+    <div>
+      <h1>Clients Project Page</h1>
+    </div>
+  );
+}
+
+export default ClientsProjectPage;
